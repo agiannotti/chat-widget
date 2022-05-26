a99b3e42937c28f90b23268b4fa7ae2c0e64b684
agiannotti <aj.gianno@gmail.com>
2022-05-25 21:02:12 -0500

    removed createRoot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
+// Find all widget divs
 const widgetDivs = document.querySelectorAll(".chat_widget");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-widgetDivs.forEach(() => {
-  root.render(
-    <App />
+
+// Inject our React App into each class
+widgetDivs.forEach((div) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    div
   );
 });
