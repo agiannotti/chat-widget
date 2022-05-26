14acada684b2bf17714508a9045f99650c28a2a5
agiannotti <aj.gianno@gmail.com>
2022-05-25 23:00:37 -0500

    adjust

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { addResponseMessage, Widget } from 'react-chat-widget';
+import './styles.css'
 import 'react-chat-widget/lib/styles.css';
 import { v4 as uuidv4 } from 'uuid';
 import { getBot } from './http/getBot';
-
+import './App.js'
 
 function App() {
   const uuid = uuidv4();
@@ -22,7 +23,12 @@ function App() {
       <Widget
         handleNewUserMessage={handleNewUserMessage}
         title="x-chat-ui"
-        subtitle="embedded chat" />
+        subtitle="embedded chat"
+        // emojis="true"
+        resizable="true"
+      // profileAvatar="true"
+      />
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 
-// Find all widget divs
 const root = ReactDOM.createRoot(document.getElementById('chat_widget'));
 root.render(
   <App />
